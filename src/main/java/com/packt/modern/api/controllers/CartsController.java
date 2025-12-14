6baaa24c7161d79120802e60a4f4da574cf5e9a9
JOYWORK-IDEA\shootinger <shootinger@naver.com>
2025-12-15 01:46:13 +0900

    4장까지 적용

--- src/main/java/com/packt/modern/api/controllers/CartsController.java
+++ src/main/java/com/packt/modern/api/controllers/CartsController.java
@@ -1,14 +1,15 @@
-package com.packt.modern.api.controllers;
+package com.packt.modern.api.controller;
 
+import static org.springframework.http.ResponseEntity.accepted;
 import static org.springframework.http.ResponseEntity.ok;
 
 import com.packt.modern.api.CartApi;
+import com.packt.modern.api.hateoas.CartRepresentationModelAssembler;
 import com.packt.modern.api.model.Cart;
 import com.packt.modern.api.model.Item;
-import java.util.Collections;
-import java.util.List;
-
+import com.packt.modern.api.service.CartService;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -16,48 +17,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : github.com/sharmasourabh
- * @project : Chapter03 - Modern API Development with Spring and Spring Boot Ed 2
- * @created : 31/10/2022, Monday
+ * @project : Chapter04 - Modern API Development with Spring and Spring Boot Ed 2
  **/
 @RestController
 public class CartsController implements CartApi {
 
   private static final Logger log = LoggerFactory.getLogger(CartsController.class);
+  private final CartService service;
+  private final CartRepresentationModelAssembler assembler;
+
+  public CartsController(CartService service, CartRepresentationModelAssembler assembler) {
+    this.service = service;
+    this.assembler = assembler;
+  }
 
   @Override
   public ResponseEntity<List<Item>> addCartItemsByCustomerId(String customerId, @Valid Item item) {
     log.info("Request for customer ID: {}\nItem: {}", customerId, item);
-    return ok(Collections.emptyList());
+    return ok(service.addCartItemsByCustomerId(customerId, item));
   }
 
   @Override
   public ResponseEntity<List<Item>> addOrReplaceItemsByCustomerId(String customerId,
       @Valid Item item) {
-    return null;
+    return ok(service.addOrReplaceItemsByCustomerId(customerId, item));
   }
 
   @Override
   public ResponseEntity<Void> deleteCart(String customerId) {
-    return null;
+    service.deleteCart(customerId);
+    return accepted().build();
   }
 
   @Override
   public ResponseEntity<Void> deleteItemFromCart(String customerId, String itemId) {
-    return null;
+    service.deleteItemFromCart(customerId, itemId);
+    return accepted().build();
   }
 
   @Override
-  public ResponseEntity<List<Cart>> getCartByCustomerId(String customerId) {
-    throw new RuntimeException("Manual Exception thrown");
+  public ResponseEntity<Cart> getCartByCustomerId(String customerId) {
+    return ok(assembler.toModel(service.getCartByCustomerId(customerId)));
   }
 
   @Override
   public ResponseEntity<List<Item>> getCartItemsByCustomerId(String customerId) {
-    return null;
+    return ok(service.getCartItemsByCustomerId(customerId));
   }
 
   @Override
-  public ResponseEntity<List<Item>> getCartItemsByItemId(String customerId, String itemId) {
-    return null;
+  public ResponseEntity<Item> getCartItemsByItemId(String customerId, String itemId) {
+    return ok(service.getCartItemsByItemId(customerId, itemId));
   }
 }
