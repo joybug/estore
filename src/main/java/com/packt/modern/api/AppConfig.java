c9e1f4673d642938c0a367fd680a13f861253c6d
조영준 <shootinger@naver.com>
2026-01-11 17:57:27 +0900

    5장 내용 반영

--- src/main/java/com/packt/modern/api/AppConfig.java
+++ src/main/java/com/packt/modern/api/AppConfig.java
@@ -2,11 +2,12 @@ package com.packt.modern.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 /**
  * @author : github.com/sharmasourabh
- * @project : Chapter04 - Modern API Development with Spring and Spring Boot Ed 2
+ * @project : Chapter05 - Modern API Development with Spring and Spring Boot Ed 2
  **/
 @Configuration
 public class AppConfig {
--- src/main/java/com/packt/modern/api/exception/GenericAlreadyExistsException.java
+++ src/main/java/com/packt/modern/api/exception/GenericAlreadyExistsException.java
@@ -1,25 +1,22 @@
 package com.packt.modern.api.exception;
 
-import java.io.Serial;
-
 /**
  * @author : github.com/sharmasourabh
- * @project : Chapter04 - Modern API Development with Spring and Spring Boot Ed 2
+ * @project : Chapter05 - Modern API Development with Spring and Spring Boot Ed 2
  **/
-public class CustomerNotFoundException extends RuntimeException {
+public class GenericAlreadyExistsException extends RuntimeException {
 
-  @Serial
   private static final long serialVersionUID = 1L;
   private final String errMsgKey;
   private final String errorCode;
 
-  public CustomerNotFoundException(ErrorCode code) {
+  public GenericAlreadyExistsException(ErrorCode code) {
     super(code.getErrMsgKey());
     this.errMsgKey = code.getErrMsgKey();
     this.errorCode = code.getErrCode();
   }
 
-  public CustomerNotFoundException(final String message) {
+  public GenericAlreadyExistsException(final String message) {
     super(message);
     this.errMsgKey = ErrorCode.CUSTOMER_NOT_FOUND.getErrMsgKey();
     this.errorCode = ErrorCode.CUSTOMER_NOT_FOUND.getErrCode();
